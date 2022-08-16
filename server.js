a0a1acc85992e0fe09e2fb0489a86fd63b3b82fe
Erikson <chris.s.erickson@gmail.com>
2022-08-15 21:31:32 -0400

    connected with db file / notes

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
 // Dependencies
 const express = require ('express');
 const fs =require('fs');
-
+const path = require ('path');
+const notes = require('./data/db.json')
 
 // setting up express server
 const app = express ();
 const PORT = process.env.PORT || 3001;
 
 // linking assets
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 // data parsing -- JSON
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 // load html 
-app.get('/', function (req, res) {
-    // res.send('does the server work?')
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+// app.get('*', function (req, res) {
+//     // res.send('does the server work?') -- Worked
+//     // res.sendFile(path.join(__dirname, './public/index.html'));
+// });
 
 // load notes html
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
+// app.get('/', function (req, res) {
+//     res.sendFile(path.join(__dirname, './public/notes.html'));
+// });
 
-
+app.get('/api/notes', (req, res) => {
+    // working -- used insomnia to pull upbody
+    res.json(notes)
+})
 
 
 
