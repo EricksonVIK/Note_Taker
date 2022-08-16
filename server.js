b4198fa0022e19726924315f35a4a8dbe4859f81
Erikson <chris.s.erickson@gmail.com>
2022-08-15 22:03:09 -0400

    heroku

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 const app = express ();
 
 // linking assets
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
 // data parsing -- JSON
 app.use(express.urlencoded({extended: true}));
